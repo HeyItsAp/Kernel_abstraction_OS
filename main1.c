904ac323174637fb79c32289caad0915cfa3032b
HeyitsAp <adrianbalunan63@gmail.com>
2026-09-09 19:04:43 +0200

    Fixed buffering issue for char c and used scanf

--- main1.c
+++ main1.c
@@ -2,10 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main(int argc, char const *argv[]){
-	int x = argv[0];
-	printf("Enter a word: \n");
-	char word = getchar(); // Waits for keyboard input.
+int main(int argc, char const *argv[]){
+	int x;
+	if (argc > 1){
+		x = atoi(argv[1]);	
+	} else {
+		exit(1);
+	}
+	char word[100];
+	printf("Enter a word:");
+	scanf("%99s", &word); // Waits for keyboard input.
+	printf("\n");
+	for (int i = 0; i < x; i++)printf("%s\n", word);
 
-	for (int i = 0; i < x; i++)printf("%c\n", word);
+	return 0;
 }
